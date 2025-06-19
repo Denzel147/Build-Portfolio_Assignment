528b7e36aafe11a36ea8041772293346eda7b959
Denzel147 <o.ayanleke@alustudent.com>
2025-06-19 12:39:17 -0700

    72c

---
+++ script.js
@@ -0,0 +1,18 @@
+// Modal functionality
+const modal = document.getElementById("contactModal");
+const btn = document.getElementById("contactBtn");
+const span = document.querySelector(".close");
+
+btn.onclick = function () {
+  modal.style.display = "flex";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
